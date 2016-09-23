366a5566d238d497a590062adf96ac09ae0d6914
Herbert Nowell <herb@herbertnowell.com>
2016-09-23 15:16:26 -0400

    Update MarketDataUtils.pm

--- market_data_grabber/MarketDataUtils.pm
+++ market_data_grabber/MarketDataUtils.pm
@@ -1,1 +1,29 @@
-##
+#============================================================================
+#
+#        USAGE: ./yahoo_scrape.pl  
+#
+#  DESCRIPTION: Utility routines for market data grabber.
+#
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Herbert Nowell (), herb@herbertnowell.com
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 09/23/2016 03:00:14 PM
+#     REVISION: ---
+#============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+use DBI;
+
+package MarketDataUtils;
+
+sub expand_exchange ()
+    {
+    # Return a list with all stock symbols associated with an exchange
+    }
+    
